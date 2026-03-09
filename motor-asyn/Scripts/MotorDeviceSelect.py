b8384828cbbe760f05244aba85b28baddf36e69a
Andrea Michelotti <andrea.michelotti@gmail.com>
2026-03-09 09:55:11 +0100

    add POI controls to MotorAsyn.bob with auto-visibility when POI PVs exist

--- motor-asyn/Scripts/MotorDeviceSelect.py
+++ motor-asyn/Scripts/MotorDeviceSelect.py
@@ -18,10 +18,18 @@ if pv:
         m_val = ""
         r_val = ""
 
+    # Set macros on the embedded container for Motor_Main.bob
     widget.setPropertyValue("file", "")
     widget.getPropertyValue("macros").add("P", p_val)
     widget.getPropertyValue("macros").add("M", m_val)
     widget.getPropertyValue("macros").add("R", r_val)
     widget.getPropertyValue("macros").add("DEVICE", pv)
     widget.setPropertyValue("file", "Motor_Main.bob")
+
+    # Also propagate P and R to the parent display so POI widgets can resolve PVs
+    parent_display = widget.getDisplayModel()
+    parent_display.getPropertyValue("macros").add("P", p_val)
+    parent_display.getPropertyValue("macros").add("R", r_val)
+    parent_display.getPropertyValue("macros").add("DEVICE", pv)
+
     logger.info("Motor device set: P=" + p_val + " M=" + m_val)
